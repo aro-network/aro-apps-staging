8ed9a0dfdea55c55d403ea241b5fea65a8b33032
picascaz <picascaz@users.noreply.github.com>
2026-04-07 09:06:39 +0000

    deploy: aro-network/aro-apps@f0601b108183af0a1393f1710d62fc490d804d2e

---
+++ desktop/_next/static/chunks/4e1090d8-7fe75a34590bb275.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[850],{9848:(t,e,r)=>{r.d(e,{irw:()=>o});var n=r(2521);function o(t){return(0,n.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polyline",attr:{points:"15 18 9 12 15 6"},child:[]}]})(t)}}}]);
